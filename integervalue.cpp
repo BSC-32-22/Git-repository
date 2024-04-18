98df940275d644641ad4177d2ebc338ac9aa7baa
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-18 23:04:23 +0200

    commit

--- integervalue.cpp
+++ integervalue.cpp
@@ -8,37 +8,9 @@ int main()
     cout << "Enter an integer value between 5 and 10\n";
     int checker = 0;
 
-    // string trey = "10";
-    // string trial;
-    // cin >> trial;
-
-    // if(trey == trial)
-    // {
-    //     cout << "yes";
-    // }
-
+    string type;
+    cin >> type;
+  
     
-    // while(checker == 0)
-    // {
-    //     string input; 
-    //     cin >> input;
-    //     char tester = input.at(0);
-    //     if (isdigit(tester)){
-
-    //         static_cast<int>(input)
-    //         if(input >= 5 && input <=10)
-    //         {
-    //         cout << "Your input value (" << input << ") has been accepted";
-    //         checker++;
-    //         }
-    //         else if(input < 5 || input > 10) 
-    //         {
-    //         cout << "You entered " << input << ". Please enter a number between 5 and 10.";
-    //         }
-    //         }
-    //     else
-    //         cout << "Sorry, you entered an invalid number, please try again";
-
-
-    // }
+    
 }
