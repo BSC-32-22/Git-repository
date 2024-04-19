d65d511498acd36a49a458776c7f6e8eda62d110
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-19 13:24:04 +0200

    further changes to filedata

--- fileData.cpp
+++ fileData.cpp
@@ -3,6 +3,10 @@
 
 using namespace std;
 
+//function prototyoe
+void vowels(string s);
+string reverse(string r);
+
 int main()
 {
     //input object
@@ -10,6 +14,43 @@ int main()
     
     //contents to string
     string fileData;
-    
+    getline(input,fileData);
+    cout << fileData;
 
+    //function for for vowels
+    vowels(fileData);
+
+    //reverse function
+    string reversedString = reverse(fileData);
+    cout << reversedString << endl;
+}
+
+void vowels(string s)
+{
+    int numberOfVowels = 0;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+        char checker = s.at(i);
+        switch (checker){
+
+            case 'a': 
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                        numberOfVowels++;
+                    
+        }
+        cout << numberOfVowels << endl;
+    }
+
+    
+        
+    
 }
