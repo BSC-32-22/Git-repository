474b71a6cab0ff7ea4a2d9954e1867be50752208
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-19 12:21:44 +0200

    change #1

---
+++ fileData.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    //input object
+    ifstream  input("fileData.txt");
+    
+    //contents to string
+    string fileData;
+    
+
+}
