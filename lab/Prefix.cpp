f0a9d3e72c751dafa5602d495aafbc3b69073081
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-22 09:50:54 +0200

    prefix final

---
+++ lab/Prefix.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int x = 3;
+
+    //value of x increased b4 value of x is assigned y
+    int y = ++x;
+
+    cout << "x: " << x << endl; //x will be 4
+    cout << "y: " << y << endl; //y will be 4
+
+    return 0;
+}
