0eaa5bd52fe00c45b89f25a404ce62f2780d5a55
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-22 10:54:37 +0200

    dynamicAllocation exercise

---
+++ lab/dynamicAllocation.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //dynamically allocating an integer
+    int* dynamicInt = new int;
+    //dynamically allocating a string
+    string* dynamicStr = new string;
+
+    cout << "Please enter the integer value: ";
+    cin >> *dynamicInt;
+
+    cout << "Please enter a String: ";
+    cin >> *dynamicStr;
+
+    //output to the console
+    cout << "Value of the integer is : " << *dynamicInt << "\n"
+         << "the string is: " << *dynamicStr << endl;
+
+}
