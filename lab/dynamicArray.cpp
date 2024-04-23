71f6d9a00b7916c9c94db177c3f61f3fc8a1abb2
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-23 02:00:11 +0200

    update

--- lab/dynamicArray.cpp
+++ lab/dynamicArray.cpp
@@ -4,5 +4,35 @@ using namespace std;
 
 int main()
 {
-    
+    int rows = 0;
+    int columns = 0;
+    int checker = 0; // for checking iteration conditions
+    double *dynamicArray = nullptr;
+
+    cout << "how many rows should the array have: ";
+    while(checker == 0)
+    {
+        cin >> rows;
+        if(rows > 3 )
+        {
+            cout << "sorry the size shouldn't exceed 3";
+        }
+        else
+            checker++;
+    }
+
+    cout << "how many columns should the array have: ";
+     while(checker == 0)
+    {
+        cin >> columns;
+        if(columns > 3 )
+        {
+            cout << "sorry the size shouldn't exceed 3";
+        }
+        else
+            checker++;
+    }
+
+    dynamicArray = new double[rows][columns];
+
 }
