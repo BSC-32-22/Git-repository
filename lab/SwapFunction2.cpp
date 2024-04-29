dcf19257d5c6ae6030688636f411c8ed7f272d27
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-29 09:57:53 +0200

    swapfunction22

--- lab/SwapFunction2.cpp
+++ lab/SwapFunction2.cpp
@@ -2,18 +2,25 @@
 
 using namespace std;
 
-void SwapNumbers(int& varA, int& varB);
+void SwapNumbers(int* varA, int* varB);
 
 int main()
 {
     int varA = 25;
     int varB = 100;
 
+    //displaying before swap
     cout << "varA before swap: " << varA << endl;
     cout << "varB before swap: " << varB << endl;
+    
+    //pointers for the variables
+    int* pPointerA = &varA;
+    int* pPointerB = &varB;
+    
+    //calling function
+    SwapNumbers(pPointerA, pPointerB);
 
-    SwapNumbers(varA, varB);
-
+    //displaying after swap
     cout << "varA after swap: " << varA << endl;
     cout << "varB after swap: " << varB << endl;
 
