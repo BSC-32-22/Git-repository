c00a1b5c9ceccd274b51f33d2e090cd5fbe07692
DBakaya <bsc-32-22@unima.ac.mw>
2024-04-30 16:19:23 +0200

    commit 2

--- lab/RectangleClassProgram.cpp
+++ lab/RectangleClassProgram.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-
-
+#include "Rectangle.h"
 
 
 int main()
--- lab/lab 7/Rectangle.cpp
+++ lab/lab 7/Rectangle.cpp
@@ -2,37 +2,35 @@
 #include "Rectangle.h"
 
     
+//defini member functions
 
 Rectangle ::Rectangle()
 {
     length = 0;
     width = 0;
 }
-
-
-//accesor methods
-        //set length
-        void Rectangle::setLength(float newLength)
-        {
-            length = newLength;
-        }
-        //set width
-        void Rectangle::setWidth(float newWidth)
-        {
-            width = newWidth;
-        }
-        //get length
-        float Rectangle::getLength()
-        {
-            return length;
-        }
-        //get width
-        float Rectangle::getWidth()
-        {
-            return width;
-        }
-        //get area
-        float Rectangle::getArea()
-        {
-            return (length * width);
-        }
+    //set length
+    void Rectangle::setLength(float newLength)
+    {
+        length = newLength;
+    }
+    //set width
+    void Rectangle::setWidth(float newWidth)
+    {
+        width = newWidth;
+    }
+   //get length
+    float Rectangle::getLength()
+    {
+           return length;
+    }
+    //get width
+    float Rectangle::getWidth()
+    {
+        return width;
+    }
+    //get area
+    float Rectangle::getArea()
+    {
+        return (length * width);
+    }
