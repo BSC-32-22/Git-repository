3888c6e0f0773804e703bbbbb3b1c17f09e90267
DBakaya <bsc-32-22@unima.ac.mw>
2024-05-03 01:26:39 +0200

    changes

--- lab/lab 7/AreaDeclaration.h
+++ lab/lab 7/AreaDeclaration.h
@@ -6,5 +6,7 @@ class Area
 {
     public:
 
-            static float AreaSquare()
-}
+            static float AreaSquare(Square squareObject);
+            static float AreaTriangle(Triangle triangleObject);
+            static float AreaCircle(Circle circleObject);
+};
