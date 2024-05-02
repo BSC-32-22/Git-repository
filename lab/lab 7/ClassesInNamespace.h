950097c97b502dd4df96ec2ed9df07939ccee0cb
DBakaya <bsc-32-22@unima.ac.mw>
2024-05-03 00:59:19 +0200

    header file

---
+++ lab/lab 7/ClassesInNamespace.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace shapes
+{
+
+    class Square
+    {
+        private:
+                float sideLength;
+        
+        public:
+                Square();
+                //overloaded
+                Square(float newSideLength);
+                //destructor
+                ~Square();
+
+                //accessor methods
+                void setSideLength(float newSideLength);
+                float getSideLength();
+    };
+
+    class Triangle
+    {
+        private:
+                float base;
+                float height;
+        public:
+                Triangle();
+                //overload
+                Triangle(float newBase, float newHeight);
+                //destructor
+                ~Triangle();
+
+                //access methods
+                void setBase(float newBase);
+                void setLength(float newLength);
+                float getBase();
+                float getLength();
+
+    };
+
+    class Circle
+    {
+        private:
+                float radius;
+        public:
+                Circle();
+                //overload
+                Circle(float radius);
+                //destructor
+                ~Circle();
+
+                //access method
+                void setRadius(float newRadius);
+                float getRadius();
+    };
+
+};
