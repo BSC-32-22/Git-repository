240edbd31856252cf88959ba4f3f81c4d159eb27
DBakaya <bsc-32-22@unima.ac.mw>
2024-05-03 02:29:26 +0200

    change

---
+++ lab/lab 7/classProgram.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "1.Calculate the area of a square\n" 
+         << "2.Calculate the area of triangle\n" 
+         << "3.Calculate the area of a circle\n"
+         << "4.Quit\n"
+         << "choose from above list: "; 
+    int choice;
+    cin >> choice;
+    //vet decision
+    switch(choice)
+        
+        case 1: Square
+    return 0;
+}
