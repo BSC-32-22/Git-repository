d8355f0f59f8352876149ef7ac11ef4216f143a5
DBakaya <bsc-32-22@unima.ac.mw>
2024-05-06 10:50:03 +0200

    done

---
+++ lab/lab 8/Person.cpp
@@ -0,0 +1,17 @@
+#include "Person.h"
+
+using namespace std;
+
+Person::Person()
+{
+    //get name
+    cout << "input first name: ";
+    cin >> mFirstName;
+    //get age
+    cout << "input age: "
+    cin >> mAge;
+    //get weight
+    cout << "input weight: ";
+    cin >> mWeight;
+   
+}
